d07f652739ad9bafd0aa839ae59fd11814087b60
aryamanarora <aryaman.arora2020@gmail.com>
2021-02-01 00:08:16 -0500

    an image :)

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,17 @@
 $(function() {
-    $('.jumbotron, .body, .footer').toggle()
-    $('.body').delay(600).slideToggle('slow')
-    // var popup = $(`<div class="toast" data-autohide="false">
-    //     <div class="toast-header">
-    //         <i class="fa fa-book mr-2" style="color: #28ADAD;"></i>
-    //         <strong class="mr-auto">Checking In</strong>
-    //         <small>December 3, 2020</small>
-    //         <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-    //             <span aria-hidden="true">&times;</span>
-    //         </button>
-    //     </div>
-    //     <div class="toast-body">
-    //         Hi tutors! If you need to check-in for community service hours logging, <a href="./check-in.html">click here for the form</a>.
-    //     </div>
-    // </div>`)
-    // $('body').append(popup)
-    // $('.toast').toast('show')
+    var popup = $(`<div class="toast" data-autohide="false">
+        <div class="toast-header">
+            <i class="fa fa-book mr-2" style="color: #28ADAD;"></i>
+            <strong class="mr-auto">Secretary Needed!</strong>
+            <small>January 31, 2020</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="toast-body">
+            We are looking for a secretary to help manage our growing numbers of students and tutors. If you are interested, please <a href="./volunteer-with-us.html">volunteer</a> and mention your interest. We will be providing community service hours in compensation.
+        </div>
+    </div>`)
+    $('body').append(popup)
+    $('.toast').toast('show')
 })
